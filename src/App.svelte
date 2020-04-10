37d46b15bbc7bf362f6f37190f5934ab511d5341
Ho Yeung (Peter) Chan <peterhyportfolio@gmail.com>
2020-04-09 23:06:47 -0700

    phrase 1 completed

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,106 @@
 <script>
-	export let name;
+  import Header from "./UI/Header.svelte";
+  import MeetupGrid from "./Meetups/MeetupGrid.svelte";
+  import TextInput from "./UI/TextInput.svelte";
+  import Button from "./UI/Button.svelte";
+
+  let title = "";
+  let subtitle = "";
+  let address = "";
+  let email = "";
+  let description = "";
+  let imageUrl = "";
+
+  let meetups = [
+    {
+      id: "e1",
+      title: "Paint SF",
+      subtitle: "May the ART to be with you",
+      description: "May the ART to be with you",
+      imageUrl:
+        "https://cdn.shopify.com/s/files/1/1136/0164/products/sanfran_grande.jpg?v=1459449289",
+      address: "123 Poke Street, San Francisco, CA 94105",
+      contact: "painsf@test.com"
+    },
+    {
+      id: "e2",
+      title: "Paint SF",
+      subtitle: "May the ART to be with you",
+      description: "May the ART to be with you",
+      imageUrl:
+        "https://cdn.shopify.com/s/files/1/1136/0164/products/sanfran_grande.jpg?v=1459449289",
+      address: "123 Poke Street, San Francisco, CA 94105",
+      contact: "painsf@test.com"
+    }
+  ];
+
+  function addMeetup() {
+    const newMeetup = {
+      id: Math.random().toString(),
+      title: title,
+      subtitle: subtitle,
+      description: description,
+      imageUrl: imageUrl,
+      contact: email,
+      address: address
+    };
+    meetups = [newMeetup, ...meetups];
+  }
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  #meetups {
+    margin-top: 5rem;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  form {
+    width: 30rem;
+    max-width: 90%;
+    margin: auto;
+  }
 </style>
+
+<Header />
+
+<main id="meetups">
+  <form on:submit|preventDefault={addMeetup}>
+    <TextInput
+      id="title"
+      label="Title"
+      type="text"
+      value={title}
+      on:input={event => (title = event.target.value)} />
+    <TextInput
+      id="subtitle"
+      label="Subtitle"
+      type="text"
+      value={subtitle}
+      on:input={event => (subtitle = event.target.value)} />
+    <TextInput
+      id="address"
+      label="Address"
+      type="text"
+      value={address}
+      on:input={event => (address = event.target.value)} />
+    <TextInput
+      id="imageUrl"
+      label="Image URL"
+      type="text"
+      value={imageUrl}
+      on:input={event => (imageUrl = event.target.value)} />
+    <TextInput
+      id="email"
+      label="E-Mail"
+      type="email"
+      value={email}
+      on:input={event => (email = event.target.value)} />
+    <TextInput
+      id="description"
+      label="Description"
+      controlType="textarea"
+      value={description}
+      on:input={event => (description = event.target.value)} />
+    <Button type="submit" caption="Save" />
+  </form>
+  <MeetupGrid {meetups} />
+</main>
---
+++ src/UI/Header.svelte
@@ -0,0 +1,22 @@
+<style>
+  header {
+    position: fixed;
+    width: 100%;
+    top: 0;
+    left: 0;
+    height: 4rem;
+    background: greenyellow;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  h1 {
+    color: white;
+    margin: 0;
+  }
+</style>
+
+<header>
+  <h1>MeetUps</h1>
+</header>
